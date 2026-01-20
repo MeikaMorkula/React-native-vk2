d5279c199e7588b621e2c96aab73756cc0a5aa01
MeikaMorkula <187277094+MeikaMorkula@users.noreply.github.com>
2026-01-20 12:29:25 +0200

    vk 2 done

--- App.tsx
+++ App.tsx
@@ -1,20 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Modal, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [modalVisibile, setModalVisible] = useState<boolean>(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.centeredView}>
+      <Modal
+        transparent={true}
+        visible={modalVisibile}
+        onRequestClose={() => {
+          setModalVisible(!modalVisibile);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+          <Text style={styles.modalText}>This is a modal</Text>
+          <Pressable onPress={() => setModalVisible(!modalVisibile)} style={[styles.button, styles.buttonClose]}>
+            <Text style={styles.textStyle}>Close</Text>
+          </Pressable>
+          <StatusBar style="auto" />
+        </View>
+        </View>
+        
+      </Modal>
+
+      <Pressable
+        style={[styles.button, styles.buttonOpen]}
+        onPress={() => setModalVisible(true)}
+      >
+        <Text>Click to show modal message</Text>
+      </Pressable>
     </View>
   );
 }
 
+//Tyylit otettu https://reactnative.dev/docs/modal esimerkistä
 const styles = StyleSheet.create({
-  container: {
+  centeredView: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
   },
 });
